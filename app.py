8408d5c3b79ef7f49e7661d90bdcef08dbaa2bb2
Rafael-Fernandes314 <rafaelfernandesjp@gmail.com>
2026-07-15 18:31:04 -0300

    flask-login e autenticação de usuários

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import sqlite3
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager,UserMixin,login_user,logout_user,login_required,current_user
 
 app = Flask(__name__)
+app.secret_key = 'batata'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class User(UserMixin):
+    def __init__(self, id, nome, email):
+        self.id = id
+        self.nome = nome
+        self.email = email
 
 def iniciar_banco():
     conexao = sqlite3.connect('banco.db')
@@ -11,8 +23,8 @@ def iniciar_banco():
         CREATE TABLE IF NOT EXISTS usuarios ( 
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nome TEXT NOT NULL,
-            email TEXT NOT NULL,
-            senha VARCHAR(20) NOT NULL UNIQUE) 
+            email TEXT NOT NULL UNIQUE,
+            senha VARCHAR(20) NOT NULL) 
     """) 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS lembretes ( 
@@ -30,14 +42,15 @@ def obter_conexao():
     conexao.row_factory = sqlite3.Row
     return conexao
 
-
-def obter_usuario(usuario_id):
-    if not usuario_id:
-        return None
+@login_manager.user_loader
+def load_user(user_id):
     conexao = obter_conexao()
-    usuario = conexao.execute("SELECT nome FROM usuarios WHERE id = ?", (usuario_id,)).fetchone()
+    usuario = conexao.execute("SELECT * FROM usuarios WHERE id = ?",(user_id,)).fetchone()
     conexao.close()
-    return usuario['nome'] if usuario else None
+
+    if usuario:
+        return User(usuario["id"],usuario["nome"],usuario["email"])
+    return None
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -59,35 +72,33 @@ def cadastro():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    mensagem = request.args.get('mensagem')
+    if current_user.is_authenticated:
+        return redirect(url_for("inicio"))
+    mensagem = request.args.get("mensagem")
 
     if request.method == "POST":
         email = request.form.get("email")
-        senha = request.form.get("senha") 
-        
+        senha = request.form.get("senha")
+
         conexao = obter_conexao()
-        usuario = conexao.execute("SELECT * FROM usuarios WHERE email = ? AND senha = ?", (email, senha)).fetchone()
+        usuario = conexao.execute("SELECT * FROM usuarios WHERE email = ? AND senha = ?",(email, senha)).fetchone()
         conexao.close()
 
         if usuario:
-            resp = make_response(redirect(url_for("inicio")))
-            resp.set_cookie("user_id", str(usuario['id']), httponly=True)
-            return resp
+            user = User(usuario["id"], usuario["nome"], usuario["email"])
+            login_user(user)
+            return redirect(url_for("inicio"))
         return render_template("login.html", mensagem="login_invalido")
 
     return render_template("login.html", mensagem=mensagem)
 
-
 @app.route("/inicio")
+@login_required
 def inicio():
-    user_id = request.cookies.get('user_id')
-    nome = obter_usuario(user_id)
-    if nome is None:
-        return redirect(url_for("login", mensagem="precisa_logar"))
-    return render_template("inicio.html", nome=nome)
-
+    return render_template("inicio.html", nome=current_user.nome)
 
 @app.route("/ver")
+@login_required
 def ver():
     conexao = obter_conexao()
     conexao.row_factory = sqlite3.Row
@@ -97,6 +108,7 @@ def ver():
 
 
 @app.route("/criar", methods=["GET", "POST"])
+@login_required
 def criar():
     if request.method == "POST":
         titulo = request.form.get("titulo")
@@ -113,6 +125,7 @@ def criar():
 
 
 @app.route("/editar/<int:id>", methods=["GET", "POST"])
+@login_required
 def editar(id):
     conexao = obter_conexao()
 
@@ -131,6 +144,7 @@ def editar(id):
 
 
 @app.route("/excluir/<int:id>")
+@login_required
 def excluir(id):
     conexao = obter_conexao()
     conexao.execute("DELETE FROM lembretes WHERE id = ?", (id,))
@@ -140,10 +154,10 @@ def excluir(id):
 
 
 @app.route("/logout")
+@login_required
 def logout():
-    resp = make_response(redirect(url_for("login")))
-    resp.delete_cookie("user_id")
-    return resp
+    logout_user()
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
